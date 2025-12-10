95bd0c9c0076d098ef1a6daaa249f66a0330ce40
Aleksander Laasmägi <allaas@taltech.ee>
2025-12-10 09:05:38 +0200

    Updates

---
+++ DataAccessLayer/IRepository.cs
@@ -0,0 +1,28 @@
+using Entities;
+
+namespace Repository;
+
+public interface IRepository
+{
+    // CREATE methods
+    bool CreateCurriculum(Curriculum curriculum);
+    bool CreateSubject(Subject subject);
+    
+    // READ methods
+    Curriculum? GetCurriculumById(Guid curriculumId);
+    List<Curriculum> GetAllCurriculums();
+    List<Curriculum> GetCurriculumsBySubject(Guid subjectId);
+    Subject? GetSubjectById(Guid subjectId);
+    List<Subject> GetAllSubjects();
+    List<Subject> GetSubjectsByCurriculum(Guid curriculumId);
+    
+    // UPDATE methods
+    bool UpdateCurriculum(Guid curriculumId, Curriculum curriculum);
+    bool UpdateSubject(Guid subjectId, Subject subject);
+    bool AddSubjectToCurriculum(Guid curriculumId, Guid subjectId);
+    bool RemoveSubjectFromCurriculum(Guid curriculumId, Guid subjectId);
+    
+    // DELETE methods
+    bool DeleteCurriculum(Guid curriculumId);
+    bool DeleteSubject(Guid subjectId);
+}
